38a195215daac2db1c7a9e2378b8eec279bb5218
M. Fay Wu <mfaywu@ucla.edu>
2016-05-27 15:56:01 -0700

    Add enpoints with twoefay

--- hello/views.py
+++ hello/views.py
@@ -13,10 +13,9 @@ from .forms import LoginForm
 from .models import Customer
 
 def index(request):
-    #return HttpResponse('Hello from Python!')
-    #r = requests.post('http://104.236.75.160:4000'){'username':'<username here>','email':'<email here>','phone':'<phone # here>'} 
-    #print r.text
-    return render(request, 'index.html')
+    r = requests.post('http://twoefay.xyz:8080/register', json={'username' : 'test', 'email': 'email', 'phone':'phone'})
+    print (r.json()['authenticated'])
+    return HttpResponse('hi')
     #r = requests.get('https://lh6.ggpht.com/8R2VbnmJNqIHQZDB9gJ5FgnYlFcUO1c14BRQT2yahNKIDo1AXryjqX2waWt2cU-GPw=w300')
     #print r.text
     #return HttpResponse('<pre>' + r.text + '</pre>')
@@ -54,7 +53,7 @@ def customer_new (request, template='customer_new.html'):
             
             try:
                 #r = requests.post('https://twoefay.xyz:8080/register', verify=False, json={'username':username, 'email':email, 'phone':phone})     
-                r = requests.post('http://104.236.75.160:10000', json={'username':username, 'email':email, 'phone':phone})
+                r = requests.post('http://test.twoefay.xyz/register', json={'username':username, 'email':email, 'phone':phone})
                 json_response = r.json()
                 print (json_response['token'])
                 token = json_response['token']
@@ -62,12 +61,9 @@ def customer_new (request, template='customer_new.html'):
                 print ("Signup connect timeout")
             except requests.exceptions.ReadTimeout as e:
                 print ("Signup read timeout")
-            #except requests.exceptions.RequestException as e:
-            #    print ("Signup request exception")
+            except requests.exceptions.RequestException as e:
+                print ("Signup request exception")
                 
-            #TODO For PROD environment
-            #r = requests.post('http://twoefay.xyz:8080/register', json={'username':username, 'email':email, 'phone':phone})
-
             customer = Customer(username=username, password=password, email=email, phone=phone, token=token)
             customer.save()
             
@@ -114,25 +110,33 @@ def login(request, template='login.html'):
             else:
                 if password == customer.password:
                     if customer.token == '':
-                        return HttpResponse('successful sign in but no twoefay')
+                        return HttpResponse('successful one-factor twoefay.')
+                        #TODO Include link to sign up for two-factor on a one-factor account
                     else:
                         verified = 'False'
                         try:
-                            r = requests.post('http://104.236.75.160:10000', json={'token':customer.token})
+                            r = requests.post('http://test.twoefay.xyz/login', json={'token':customer.token})
                             json_response = r.json()
-                            verified = json_response['verified']
-                            print('verified' + verified)
+                            verified = json_response['login']
+                            print('login success/failure/unverified: ' + verified)
                         except requests.exceptions.ReadTimeout as e:
                             print ("Login read timeout")
                         except requests.exceptions.ConnectTimeout as e:
                             print ("Login connect timeout")
+                        except requests.exceptions.RequestException as e:
+                            print ("Login request exception")
 
-                        if verified == 'True':
-                            return HttpResponse('successful sign in')
-                        else: 
-                            #TODO Want to have backup method
-                            return HttoResponse('unsuccessful twoefay sign in')
-                    #return HttpResponseRedirect('success.html')
+                        if verified == 'success':
+                            return HttpResponse('successful sign in with twoefay')
+                        elif verified == 'failure': 
+                            return HttpResponse('failure sign in with twoefay: you didnt authenticate with app, but you have one installed on your phone')
+                        elif verified == '':
+                            return HttpResponse('failure sign in with twoefay: twoefay server is down')
+                        elif verified == 'unverified':
+                            #TODO Go to backup
+                            return HttpResponse('unverified user with twoefay: go to backup')
+                        else:
+                            return HttpResponse('ERROR: This should not happen ever.')
                 else:
                     return HttpResponse('password is incorrect!')
         else: 
