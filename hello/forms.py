5cfe61822323131e9198343b8c4a6e6b60eadd47
M. Fay Wu <mfaywu@ucla.edu>
2016-05-31 10:38:15 -0700

    Password hiding

--- hello/forms.py
+++ hello/forms.py
@@ -4,13 +4,13 @@ from .models import Customer
 
 class CustomerForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField()
-    email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    email = forms.EmailField()
     phone = forms.CharField()
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
 
 class BackupForm(forms.Form):
     otp = forms.CharField()
